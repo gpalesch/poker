2a4f9a29ee31be6102dee936fa4e5ffa605032d8
Gina Palesch <gishpa@live.com>
2026-03-05 15:05:26 +0100

    update junit 5 and assertj, first test

---
+++ jocker/src/test/java/com/example/CardTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+public class CardTest {
+    @Test
+    void cardRankAndSuit() {
+        Card card = new Card("Ace", "Spades");
+        assertThat(card.getRank()).isEqualTo("Ace");
+        assertThat(card.getSuit()).isEqualTo("Spades");
+    }
+    
+}
